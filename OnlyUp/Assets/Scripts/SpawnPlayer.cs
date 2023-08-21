7a8e7b51f967e99b5a3fe5c56f98de93bb3ea6fc
VladNaboka <naboka.wlad@mail.ru>
2023-08-21 13:24:02 +0600

    fixed ladder

--- OnlyUp/Assets/Scripts/SpawnPlayer.cs
+++ OnlyUp/Assets/Scripts/SpawnPlayer.cs
@@ -4,21 +4,13 @@ using UnityEngine;
 
 public class SpawnPlayer : MonoBehaviour
 {
-    //public static Vector3 playerSpawnPosition = new Vector3(-0.4f, 6.7f, -0.79f);
-    public Vector3 playerPos;
+    [SerializeField] private Vector3 playerPos;
     public GameObject player;
 
     private void Awake()
     {
-        //if (playerSpawnPosition != null)
-        //{
-            
-        //    //player.transform.position = playerSpawnPosition;
-        //}
         Instantiate(player, new Vector3(PlayerPrefs.GetFloat("posX"),
             PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ")), Quaternion.identity);
-        //player.transform.position = new Vector3(PlayerPrefs.GetFloat("posX"),
-        //    PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ"));
     }
 
     //private void OnTriggerEnter(Collider other)
@@ -38,14 +30,10 @@ public class SpawnPlayer : MonoBehaviour
         Debug.Log("new pos");
         player.transform.position = new Vector3(PlayerPrefs.GetFloat("posX"),
             PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ"));
-        //player.transform.position = new Vector3(PlayerPrefs.GetFloat("posX"), 
-        //    PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ"));
     }
     private void Update()
     {
         playerPos = new Vector3(PlayerPrefs.GetFloat("posX"),
             PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ"));
-    //    playerPos = new Vector3(PlayerPrefs.GetFloat("posX"),
-    //PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ"));
     }
 }
