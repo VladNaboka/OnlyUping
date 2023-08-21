4fcc1e21e3e636d08ab12f848e9a69954341c964
VladNaboka <naboka.wlad@mail.ru>
2023-08-21 13:18:26 +0600

    fix bags

--- OnlyUp/Assets/Scripts/SpawnPlayer.cs
+++ OnlyUp/Assets/Scripts/SpawnPlayer.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class SpawnPlayer : MonoBehaviour
 {
-    public static Vector3 playerSpawnPosition = new Vector3(-0.4f, 6.7f, -0.79f);
+    //public static Vector3 playerSpawnPosition = new Vector3(-0.4f, 6.7f, -0.79f);
     public Vector3 playerPos;
     public GameObject player;
 
     private void Awake()
     {
-        if (playerSpawnPosition != null)
-        {
-            Instantiate(player, new Vector3(PlayerPrefs.GetFloat("posX"),
+        //if (playerSpawnPosition != null)
+        //{
+            
+        //    //player.transform.position = playerSpawnPosition;
+        //}
+        Instantiate(player, new Vector3(PlayerPrefs.GetFloat("posX"),
             PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ")), Quaternion.identity);
-            //player.transform.position = playerSpawnPosition;
-        }
         //player.transform.position = new Vector3(PlayerPrefs.GetFloat("posX"),
         //    PlayerPrefs.GetFloat("posY"), PlayerPrefs.GetFloat("posZ"));
     }
